ae28bed5858b4a4da96e5221961e16691c956313
digitopoi <hubo85@gmail.com>
2018-01-16 06:03:53 -0600

    Check for null username

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -28,7 +28,8 @@ namespace DatingApp.API.Controllers
         [HttpPost ("register")]
         public async Task<IActionResult> Register ([FromBody] UserForRegisterDto userForRegisterDto)
         {
-            userForRegisterDto.Username = userForRegisterDto.Username.ToLower ();
+            if (!string.IsNullOrEmpty(userForRegisterDto.Username))
+                userForRegisterDto.Username = userForRegisterDto.Username.ToLower ();
 
             if (await _repo.UserExists (userForRegisterDto.Username))
                 ModelState.AddModelError ("Username", "Username already exists");
