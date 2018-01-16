ecb923e64391f6849766d14dbe24df64e68543ce
digitopoi <hubo85@gmail.com>
2018-01-15 18:54:49 -0600

    add user register dto

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,7 +1,40 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DatingApp.API.Controllers
 {
-    public class AuthController
+    [Route ("api/[controller]")]
+    public class AuthController : Controller
     {
-        
+        private readonly IAuthRepository _repo;
+
+        public AuthController (IAuthRepository repo)
+        {
+            this._repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto) 
+        {
+            //  TODO: validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists.");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
     }
 }
