10ed5f6ef2c0e15644c4c284ea0dad7668e306b5
digitopoi <hubo85@gmail.com>
2018-01-30 18:29:58 -0600

    extend user model, add photo model

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,6 +7,7 @@ namespace DatingApp.API.Data
     {
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photos { get; set; }
         
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     }
