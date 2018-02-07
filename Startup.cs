fb644597bc50ca2dd4ab7b0677c9de08d4ec5aa7
digitopoi <hubo85@gmail.com>
2018-02-07 17:32:55 -0600

    Add cloudinary configuration

--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,8 @@ namespace DatingApp.API
 
             services.AddCors();
 
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
+
             services.AddAutoMapper();
 
             services.AddScoped<IAuthRepository, AuthRepository>();
