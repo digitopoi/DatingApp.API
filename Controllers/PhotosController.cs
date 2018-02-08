c30e6a574dfafd8377bbb24a66653c04de3a0df8
digitopoi <hubo85@gmail.com>
2018-02-07 21:48:06 -0600

    Crop photo on upload

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -74,7 +74,8 @@ namespace DatingApp.API.Controllers
                 {
                     var uploadParams = new ImageUploadParams()
                     {
-                        File = new FileDescription(file.Name, stream)
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation().Width(500).Height(500).Crop("fill").Gravity("face")
                     };
 
                     uploadResult = _cloudinary.Upload(uploadParams);
