81c559373e7a3d85671247e877bb0cb27bea253a
digitopoi <hubo85@gmail.com>
2018-02-08 15:03:20 -0600

    Update register endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -43,14 +43,13 @@ namespace DatingApp.API.Controllers
             if (!ModelState.IsValid)
                 return BadRequest (ModelState);
 
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register (userToCreate, userForRegisterDto.Password);
 
-            return StatusCode (201);
+            var userToReturn = _mapper.Map<UserForDetailDto>(createdUser);
+
+            return CreatedAtRoute("GetUser", new { controller = "Users", id = createdUser.Id }, userToReturn);
         }
 
         [HttpPost ("login")]
