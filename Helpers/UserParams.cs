385e8667fb6aa70e039eeda79adb32a3b6489dbf
digitopoi <hubo85@gmail.com>
2018-02-12 10:24:52 -0600

    Add User Params class

---
+++ Helpers/UserParams.cs
@@ -0,0 +1,15 @@
+namespace DatingApp.API.Helpers
+{
+    public class UserParams
+    {
+        public int PageNumber { get; set; } = 1;
+        private const int  MaxPageSize = 50;
+        private int pageSize = 10;
+
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+    }
+}
