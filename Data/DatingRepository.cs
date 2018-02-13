184cf732d17fb16b36234385663a8404ef9316e2
digitopoi <hubo85@gmail.com>
2018-02-13 11:59:56 -0600

    Filter by age

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -50,7 +50,12 @@ namespace DatingApp.API.Data
 
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
-            
+
+            if (userParams.MinAge != 18 || userParams.MaxAge != 99) {
+                users = users.Where(u => u.DateOfBirth.CalculateAge() >= userParams.MinAge
+                                    && u.DateOfBirth.CalculateAge() <= userParams.MaxAge);
+            }
+
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
