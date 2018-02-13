6729bb43247a81c9ae4a6c5f638c2f27c5dd55d9
digitopoi <hubo85@gmail.com>
2018-02-13 17:49:10 -0600

    Add like dbset, configuration, and migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,7 +8,26 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
         
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(k => new { k.LikerId, k.LikeeId } );
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Liker)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likee)
+                .HasForeignKey(u => u.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
