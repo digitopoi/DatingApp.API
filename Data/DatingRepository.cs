ffe62d90ec8196d313123479450fc3fee0c2bb84
digitopoi <hubo85@gmail.com>
2018-02-13 23:23:03 -0600

    Add message thread endpoint

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -40,7 +40,7 @@ namespace DatingApp.API.Data
             return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
         }
 
-        public Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
+        public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
         {
             var messages = _context.Messages
                 .Include(u => u.Sender).ThenInclude(p => p.Photos)
@@ -62,12 +62,20 @@ namespace DatingApp.API.Data
 
             messages = messages.OrderByDescending(d => d.DateSent);
 
-            return PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+            return await PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
-            throw new System.NotImplementedException();
+            var messages = await _context.Messages
+                .Include(u => u.Sender).ThenInclude(p => p.Photos)
+                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+                .Where(m => (m.RecipientId == userId && m.SenderId == recipientId) ||
+                    (m.RecipientId == recipientId && m.SenderId == userId))
+                .OrderByDescending(m => m.DateSent)
+                .ToListAsync();
+
+            return messages;
         }
 
         public async Task<Photo> GetPhoto(int id)
